d714f98c978e1417913de86508f68ebfd56fd527
Arthur Henrique Cavalcanti <50219237+Stuepp@users.noreply.github.com>
2021-11-16 21:46:08 -0300

    Add files via upload

---
+++ Curso/ExemploPackage/src/com/empresa/entities/Player.java
@@ -0,0 +1,5 @@
+package com.empresa.entities;
+
+public class Player {
+
+}
---
+++ Curso/ExemploPackage/src/com/empresa/graphics/Main.java
@@ -0,0 +1,8 @@
+package com.empresa.graphics;
+import com.empresa.entities.*;//* importa todas as classes!
+
+public class Main {
+	public static void main(String[]args) {
+		new Player();
+	}
+}
